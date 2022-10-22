306db6295653a0651ae0987cb5b10d9653befd47
putinu <putinu2521@gmail.com>
2022-10-22 10:42:56 +0900

    ButtonBase抽象クラス作成

---
+++ Assets/Scripts/Buttons/ButtonBase.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonBase : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    /// <summary>
+    /// オブジェクトをクリックしたとき
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+    /// <summary>
+    /// オブジェクトにカーソルが入った時
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+    }
+
+    /// <summary>
+    /// オブジェクトからカーソルが出た時
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+}
