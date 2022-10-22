957b3b9b037281064cf0f08c812a90f4d6040f35
putinu <putinu2521@gmail.com>
2022-10-22 12:20:13 +0900

    各種ボタンの実装

---
+++ Assets/Scripts/Buttons/BackToStageButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BackToStageButton : ButtonBase
+{
+    [SerializeField] private InGameProcessor inGameProcessor;
+    
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        inGameProcessor.GoBack();
+    }
+}
---
+++ Assets/Scripts/Buttons/StageSelectButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StageSelectButton : ButtonBase
+{
+    [SerializeField] private int stage;
+    [SerializeField] private SceneChanger sceneChanger;
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        InGameProcessor.GoStage(stage);
+        sceneChanger.LoadScene($"Stage{stage}");
+    }
+}
