5cbbcd8e05d22f4d9d32d3c68ae79875e10d76ef
putinu <putinu2521@gmail.com>
2022-10-22 17:16:33 +0900

    タイトル画面での音楽再生命令記述

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Title
+{
+    public class TitleManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            AudioManager.instance.PlayBGM(0);
+        }
+    }
+}
