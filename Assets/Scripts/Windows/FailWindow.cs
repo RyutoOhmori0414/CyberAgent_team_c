1a1adcfd05abcbd189108c35806f91a0add4ac2f
putinu <putinu2521@gmail.com>
2022-10-22 12:14:37 +0900

    StageClearWindowとFailWindowの仮実装

---
+++ Assets/Scripts/Windows/FailWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FailWindow : Window
+{
+    [SerializeField] private GameObject windowObject;
+        
+    public override void Open()
+    {
+        windowObject.SetActive(true);
+    }
+
+    public override void Close()
+    {
+        windowObject.SetActive(false);
+    }
+}
